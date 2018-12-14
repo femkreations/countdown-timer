96d3f2bf3a9be75a7b5d11fcac4b2d886fc28f50
Femy Praseeth <femkreations@gmail.com>
2018-12-14 14:43:22 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html> 
+<html>
+<head>
+	<?php include("../inc/meta.php") ?>
+    <link rel="stylesheet" href="./style.css">
+    <title>Countdown</title>
+  
+	<?php include("../inc/header.php") ?>
+    
+	<h1><span>Countdown Timer</span></h1>
+	</div>
+
+<div id="canvas" class="container group">
+    
+    <div id="primaryContent" class="group">
+    	<p>Input the time in minutes and click start</p>
+	    <div id="inputArea"></div>
+		<h2 id="time">0:00</h2>
+        
+        
+    </div> <!-- main content -->
+    <!--
+    <div id="secondaryContent">
+    
+    
+    </div><!-- right col content --> 
+    
+    </div>
+   
+   
+	<?php include("../inc/footer.php") ?> 
+    
+    
+    
+    
+<script src="script.js"></script>
+</body>
+</html>
